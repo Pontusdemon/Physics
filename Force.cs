9d20ac1d7d9a9d4bdefcd6dc6ff085f311e16949
Pontus Ling <pontus.ling@learnet.se>
2021-11-19 14:23:16 +0100

    w

--- Force.cs
+++ Force.cs
@@ -10,19 +10,7 @@ namespace Physics
     {
         public class Inertia : Force
         {
-            public double Net;
-            public double F1;
-            public double F2;
 
-            public void InertiaNet()
-            {
-                Net = F1 + F2;
-
-                if (Net == 0)
-                {
-                    Console.WriteLine("there is no change in inertia");
-                }
-            }
         }
     }
 }
--- ForceSelector.cs
+++ ForceSelector.cs
@@ -13,23 +13,6 @@ namespace Physics
             Console.WriteLine("select force type");
             string ForceType = Console.ReadLine();
 
-            if (ForceType == "inertia")
-            {
-                var GetInertia = new Force.Inertia();
-                Console.WriteLine("select var");
-                string InertiaVar = Console.ReadLine();
-
-                if (InertiaVar == "Net")
-                {
-                    Console.WriteLine("enter F1");
-                    GetInertia.F1 = double.TryParse(Console.ReadLine(), out double F1) ? F1 : 0;
-
-                    Console.WriteLine("enter F2");
-                    GetInertia.F2 = double.TryParse(Console.ReadLine(), out double F2) ? F2 : 0;
-
-                    GetInertia.InertiaNet();
-                }
-            }
         }
     }
 }
