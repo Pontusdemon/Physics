b32c45b2e686aff3d42229527a3900c4d4460b4c
Pontus Ling <pontus.ling@learnet.se>
2021-11-19 13:00:40 +0100

    W

--- Force.cs
+++ Force.cs
@@ -14,20 +14,9 @@ namespace Physics
             public double F1;
             public double F2;
 
-            public double dv;
-            public double dt;
-            public double ds;
-
-            public double x = 0;
-
             public void InertiaNet()
             {
                 Net = F1 + F2;
-                Console.WriteLine("inertia = " + Net);
-                if (Net == 0)
-                {
-
-                }
             }
         }
     }
