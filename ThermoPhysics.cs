c950a6ef829494b527541b1ec7cd6e762c907b21
Pontus Ling <pontus.ling@learnet.se>
2021-12-14 13:25:45 +0100

    14/12

--- ThermoPhysics.cs
+++ ThermoPhysics.cs
@@ -29,7 +29,7 @@ namespace Physics
                 public double V;
 
                 public override void MasterDensity()
-                    // d = m / V
+                // d = m / V
                 {
                     d = m / V;
                     Console.WriteLine("density = " + d);
@@ -45,7 +45,7 @@ namespace Physics
                 public double h;
 
                 public override void MasterDensity()
-                    // d = p / (g * h)
+                // d = p / (g * h)
                 {
                     d = p / (g * h);
                     Console.WriteLine("density = " + d);
@@ -61,7 +61,7 @@ namespace Physics
                 public double V;
 
                 public override void MasterDensity()
-                    // d = FL / (g * V)
+                // d = FL / (g * V)
                 {
                     d = FL / (g * V);
                     Console.WriteLine("density = " + d);
@@ -69,238 +69,245 @@ namespace Physics
             }
         }
 
-
-
-
-
-
-       /* 
-        public class Pressure : ThermoPhysics
+        public abstract class Pressure : ThermoPhysics
         {
-            public double p;
-            public double x;
+            public abstract void MasterPressure();
 
-            public double F;
-            public double A;
-            public double d;
-            public double g;
-            public double h;
-            public double FL;
-            public double V;
-            public double k;
-            public double T;
 
-            // p = F / A
-            // P = x + dgh
-            // p = FL / (g * V)
-            // p = (k * T) / V
-
-            public void DefinedPressure()
-            // p = F / A
-            {
-                Console.WriteLine("p = F / A");
-
-                p = F / A;
-                Console.WriteLine("defined pressure = " + p);
-            }
-
-            public void FluidPressure()
-            // p = x + dgh
-            {
-                Console.WriteLine("p = x + dgh");
-
-                p = x + (d * g * h);
-                Console.WriteLine("fluid pressure = " + p);
-            }
-
-            public void ExternalPressureByFluidPressure()
-            // x = p - dgh
-            {
-                Console.WriteLine("x = p - dgh");
-
-                x = p - (d * g * h);
-                Console.WriteLine("external pressure = " + x);
-            }
-
-            public void PressureByArkimedes()
-            // p = FL / (gV)
-            {
-                Console.WriteLine("p = F / gV");
-
-                p = FL / (g * V);
-                Console.WriteLine("pressure = " + p);
-            }
-
-            public void PressureByGeneralGaslaw()
-            // p = (k * T) / V;
-            {
-                Console.WriteLine("p = kT / V");
-                
-                p = (k * T) / V;
-                Console.WriteLine("gas pressure = " + p);
-            }
         }
 
-        public class Force : ThermoPhysics
-        {
-            // pressure force
-            // lift force
-            // inertia
 
-            public double F;
-            public double FL;
 
-            public double p;
-            public double A;
-            public double g;
-            public double V;
 
-            public void PressureForce()
-            // F = p * A
-            {
-                Console.WriteLine("F = p * A");
 
-                F = p * A;
-                Console.WriteLine("pressure force = " + F);
-            }
 
-            public void LiftForceByArkimedes()
-            // FL = pgV
-            {
-                Console.WriteLine("FL = pgV");
+        /* 
+         public class Pressure : ThermoPhysics
+         {
+             public double p;
+             public double x;
 
-                FL = p * g * V;
-                Console.WriteLine("lift force = " + FL);
-            }
-        }
+             public double F;
+             public double A;
+             public double d;
+             public double g;
+             public double h;
+             public double FL;
+             public double V;
+             public double k;
+             public double T;
 
-        public class Volume : ThermoPhysics
-        {
-            public double V;
+             // p = F / A
+             // P = x + dgh
+             // p = FL / (g * V)
+             // p = (k * T) / V
 
-            public double d;
-            public double m;
-            public double g;
-            public double FL;
-            public double k;
-            public double T;
-            public double p;
+             public void DefinedPressure()
+             // p = F / A
+             {
+                 Console.WriteLine("p = F / A");
 
-            public void VolumeByDensity()
-            // V = m / d
-            {
-                Console.WriteLine("V = m / d");
+                 p = F / A;
+                 Console.WriteLine("defined pressure = " + p);
+             }
 
-                V = m / d;
-                Console.WriteLine("volume = " + V);
-            }
+             public void FluidPressure()
+             // p = x + dgh
+             {
+                 Console.WriteLine("p = x + dgh");
 
-            public void volumeByArkimedes()
-            // V = FL / (dg)
-            {
-                Console.WriteLine("V = FL / (d * g)");
+                 p = x + (d * g * h);
+                 Console.WriteLine("fluid pressure = " + p);
+             }
 
-                V = FL / (d * g);
-                Console.WriteLine("volume  = " + V);
-            }
+             public void ExternalPressureByFluidPressure()
+             // x = p - dgh
+             {
+                 Console.WriteLine("x = p - dgh");
 
-            public void VolumeByGeneralGaslaw()
-            // 
-            {
-                Console.WriteLine("V = (k * T) / p");
+                 x = p - (d * g * h);
+                 Console.WriteLine("external pressure = " + x);
+             }
 
-                V = (k * T) / p;
-                Console.WriteLine("volume = " + V);
-            }
-        }
+             public void PressureByArkimedes()
+             // p = FL / (gV)
+             {
+                 Console.WriteLine("p = F / gV");
 
-        public class CalculationMass : ThermoPhysics
-        {
-            public double m;
+                 p = FL / (g * V);
+                 Console.WriteLine("pressure = " + p);
+             }
 
-            public double d;
-            public double V;
+             public void PressureByGeneralGaslaw()
+             // p = (k * T) / V;
+             {
+                 Console.WriteLine("p = kT / V");
 
-            public void MassFunction()
-            {
-                Console.WriteLine("m = d * V");
+                 p = (k * T) / V;
+                 Console.WriteLine("gas pressure = " + p);
+             }
+         }
 
-                m = d * V;
-                Console.WriteLine("mass = " + m);
-            }
-        }
+         public class Force : ThermoPhysics
+         {
+             // pressure force
+             // lift force
+             // inertia
 
-        public class Gravity : ThermoPhysics
-        {
-            public double g;
+             public double F;
+             public double FL;
 
-            public double d;
-            public double p;
-            public double h;
-            public double FL;
-            public double V;
+             public double p;
+             public double A;
+             public double g;
+             public double V;
 
-            public void GravityExplainer()
-            {
-                Console.WriteLine("please not that g = 9. 82 in most cases but the calculations for how to prove it will be what you find now");
+             public void PressureForce()
+             // F = p * A
+             {
+                 Console.WriteLine("F = p * A");
 
-                Console.WriteLine("select calculation for g");
-                string GravityCalculation = Console.ReadLine();
-                var GetGravity = new ThermoPhysics.Gravity();
+                 F = p * A;
+                 Console.WriteLine("pressure force = " + F);
+             }
 
-                if (GravityCalculation == "fluid")
-                // g = p / (d * h)
-                {
-                    Console.WriteLine("g = p / (d * h)");
+             public void LiftForceByArkimedes()
+             // FL = pgV
+             {
+                 Console.WriteLine("FL = pgV");
 
-                    Console.WriteLine("enter fluid pressure");
-                    GetGravity.p = Convert.ToDouble(Console.ReadLine());
+                 FL = p * g * V;
+                 Console.WriteLine("lift force = " + FL);
+             }
+         }
 
-                    Console.WriteLine("enter fluid density");
-                    GetGravity.d = Convert.ToDouble(Console.ReadLine());
+         public class Volume : ThermoPhysics
+         {
+             public double V;
 
-                    Console.WriteLine("enter fluid height");
-                    GetGravity.h = Convert.ToDouble(Console.ReadLine());
+             public double d;
+             public double m;
+             public double g;
+             public double FL;
+             public double k;
+             public double T;
+             public double p;
 
-                    g = p / (d * h);
-                    Console.WriteLine("g = " + g);
-                }
+             public void VolumeByDensity()
+             // V = m / d
+             {
+                 Console.WriteLine("V = m / d");
 
-                else if (GravityCalculation == "Arkimedes")
-                // g = FL / (d * V)
-                {
-                    Console.WriteLine("g = FL / (d * V)");
+                 V = m / d;
+                 Console.WriteLine("volume = " + V);
+             }
 
-                    Console.WriteLine("enter lift force");
-                    GetGravity.FL = Convert.ToDouble(Console.ReadLine());
+             public void volumeByArkimedes()
+             // V = FL / (dg)
+             {
+                 Console.WriteLine("V = FL / (d * g)");
 
-                    Console.WriteLine("enter density");
-                    GetGravity.d = Convert.ToDouble(Console.ReadLine());
+                 V = FL / (d * g);
+                 Console.WriteLine("volume  = " + V);
+             }
 
-                    Console.WriteLine("enter volume");
-                    GetGravity.V = Convert.ToDouble(Console.ReadLine());
+             public void VolumeByGeneralGaslaw()
+             // 
+             {
+                 Console.WriteLine("V = (k * T) / p");
 
-                    g = FL / (d * V);
-                    Console.WriteLine("g = " + g);
-                }
-            }
-        }
+                 V = (k * T) / p;
+                 Console.WriteLine("volume = " + V);
+             }
+         }
 
-        public class Area : ThermoPhysics
-        {
-            public double A;
+         public class CalculationMass : ThermoPhysics
+         {
+             public double m;
 
-            public double F;
-            public double p;
+             public double d;
+             public double V;
 
-            public void AreaByDefinedPressure()
-            // A = F / p
-            {
-                Console.WriteLine("A = F / p");
+             public void MassFunction()
+             {
+                 Console.WriteLine("m = d * V");
 
-                A = F / p;
-                Console.WriteLine("defined pressure area = " + A);
-            }
-        }*/
+                 m = d * V;
+                 Console.WriteLine("mass = " + m);
+             }
+         }
+
+         public class Gravity : ThermoPhysics
+         {
+             public double g;
+
+             public double d;
+             public double p;
+             public double h;
+             public double FL;
+             public double V;
+
+             public void GravityExplainer()
+             {
+                 Console.WriteLine("please not that g = 9. 82 in most cases but the calculations for how to prove it will be what you find now");
+
+                 Console.WriteLine("select calculation for g");
+                 string GravityCalculation = Console.ReadLine();
+                 var GetGravity = new ThermoPhysics.Gravity();
+
+                 if (GravityCalculation == "fluid")
+                 // g = p / (d * h)
+                 {
+                     Console.WriteLine("g = p / (d * h)");
+
+                     Console.WriteLine("enter fluid pressure");
+                     GetGravity.p = Convert.ToDouble(Console.ReadLine());
+
+                     Console.WriteLine("enter fluid density");
+                     GetGravity.d = Convert.ToDouble(Console.ReadLine());
+
+                     Console.WriteLine("enter fluid height");
+                     GetGravity.h = Convert.ToDouble(Console.ReadLine());
+
+                     g = p / (d * h);
+                     Console.WriteLine("g = " + g);
+                 }
+
+                 else if (GravityCalculation == "Arkimedes")
+                 // g = FL / (d * V)
+                 {
+                     Console.WriteLine("g = FL / (d * V)");
+
+                     Console.WriteLine("enter lift force");
+                     GetGravity.FL = Convert.ToDouble(Console.ReadLine());
+
+                     Console.WriteLine("enter density");
+                     GetGravity.d = Convert.ToDouble(Console.ReadLine());
+
+                     Console.WriteLine("enter volume");
+                     GetGravity.V = Convert.ToDouble(Console.ReadLine());
+
+                     g = FL / (d * V);
+                     Console.WriteLine("g = " + g);
+                 }
+             }
+         }
+
+         public class Area : ThermoPhysics
+         {
+             public double A;
+
+             public double F;
+             public double p;
+
+             public void AreaByDefinedPressure()
+             // A = F / p
+             {
+                 Console.WriteLine("A = F / p");
+
+                 A = F / p;
+                 Console.WriteLine("defined pressure area = " + A);
+             }
+         }*/
     }
 }
